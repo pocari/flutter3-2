737e058af35665381eefcdeea7634ad5d2ca739a
pocari <caffelattenonsugar@gmail.com>
2019-11-07 00:36:59 +0900

    simple layout2

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,20 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Center(
-        // textDirectionを指定しないと、MaterialApp以外の場合にエラーになるらしい
-        child: Text("Hello, world!!!", textDirection: TextDirection.ltr),
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Center(
+          // textDirectionを指定しないと、MaterialApp以外の場合にエラーになるらしい
+          child: Text(
+            "Hello, world!!!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 48,
+              color: Colors.black,
+            ),
+          ),
+        ),
       );
 }
