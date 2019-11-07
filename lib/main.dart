115611fe6e1462073e31ba34fbae09b83298cfe8
pocari <caffelattenonsugar@gmail.com>
2019-11-07 13:49:32 +0900

    layout sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-          home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-            ],
-          ),
+        title: 'Pavlova Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Pavlova(),
+      );
+}
+
+class Pavlova extends StatelessWidget {
+  Widget _stars() => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.black),
+          Icon(Icons.star, color: Colors.black),
+        ],
+      );
+
+  Widget _ratings() => Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _stars(),
+            Text(
+              '170 Reviews',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'Roboto',
+                letterSpacing: 0.5,
+                fontSize: 20,
+              ),
+            )
+          ],
         ),
-      ));
+      );
+
+  Widget _iconList() {
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    return DefaultTextStyle(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.timer, color: Colors.green[500]),
+                Text('COOK:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Text('FEEDS:'),
+                Text('4-6'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _leftColumn() => Container(
+        padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+        child: Column(
+          children: <Widget>[
+            Text('titleText'),
+            Text('subTitle'),
+            _ratings(),
+            _iconList(),
+          ],
+        ),
+      );
+
+  Widget _mainWidget() => Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 440,
+            child: _leftColumn(),
+          ),
+          Image.asset('images/image03.png')
+        ],
+      );
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: _mainWidget(),
+        ),
+      );
 }
